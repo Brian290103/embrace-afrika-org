c9841ff240f10fe59a83acea6de87273f844c7c5
Brian Saitabau <briansaitabau29@gmail.com>
2025-04-27 20:34:26 +0300

    unversioned files

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+/**
+ * Creates a slug from the given string.
+ * @param str The string to slugify.
+ * @returns The slugified string.
+ */
+export function slugify(str: string): string {
+  return str
+      .toLowerCase()
+      .trim()
+      .replace(/[\s-]+/g, "-") // Replace spaces and hyphens with a single hyphen
+      .replace(/[^a-z0-9-]/g, "") // Remove all non-alphanumeric characters except hyphens
+      .replace(/^-+|-+$/g, ""); // Remove leading and trailing hyphens
+}
