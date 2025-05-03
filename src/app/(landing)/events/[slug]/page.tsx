56502fdcd34335f095102970cc3b1a888fc363af
Brian Saitabau <briansaitabau29@gmail.com>
2025-05-03 03:10:53 +0300

    updated the metadata for the event and visitation details pages

--- src/app/(landing)/events/[slug]/page.tsx
+++ src/app/(landing)/events/[slug]/page.tsx
@@ -3,10 +3,43 @@ import ClientEventDetails from "@/app/(landing)/events/[slug]/details";
 import HeaderTitle from "@/app/(landing)/components/header-title";
 import { EventsType } from "@/typings";
 import { getEventBySlug } from "@/actions/client/clientEventAction";
+import type { Metadata, ResolvingMetadata } from "next";
 
-const EventDetailsPage = async ({ params }: { params: { slug: string } }) => {
+type Props = { params: { slug: string } };
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const slug = params.slug;
+  const event: EventsType = await getEventBySlug(slug);
+
+  if (!event) {
+    return {
+      title: "Event Not Found",
+    };
+  }
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: event.title,
+    description: event.caption,
+    openGraph: {
+      title: event.title,
+      description: event.caption,
+      images: [event.featuredImageUrl, ...previousImages].filter(Boolean),
+    },
+  };
+}
+
+const EventDetailsPage = async ({ params }: Props) => {
   const event: EventsType = await getEventBySlug(params.slug);
-  console.log(event);
+
+  if (!event) {
+    return <div>Event not found</div>;
+  }
+
   return (
     <section className={"w-full flex flex-col gap-3"}>
       <HeaderTitle title={event.title} subtitle={event.caption} />
--- src/app/(landing)/visits/[slug]/page.tsx
+++ src/app/(landing)/visits/[slug]/page.tsx
@@ -3,10 +3,43 @@ import ClientVisitDetails from "@/app/(landing)/visits/[slug]/details";
 import HeaderTitle from "@/app/(landing)/components/header-title";
 import { VisitsType } from "@/typings";
 import { getVisitsBySlug } from "@/actions/visitAction";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const slug = params.slug;
+  const visit: VisitsType = await getVisitsBySlug(slug);
+
+  if (!visit) {
+    return {
+      title: "Visit Details Not Found",
+    };
+  }
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: visit.title,
+    description: visit.caption,
+    openGraph: {
+      title: visit.title,
+      description: visit.caption,
+      images: [visit.featuredImageUrl, ...previousImages].filter(Boolean), // Ensure no null/undefined
+    },
+  };
+}
 
 const VisitDetailsPage = async ({ params }: { params: { slug: string } }) => {
   const visit: VisitsType = await getVisitsBySlug(params.slug);
-  console.log(visit);
+  if (!visit) {
+    return <div>Visit not found</div>; // Basic error handling
+  }
   return (
     <section className={"w-full flex flex-col gap-3"}>
       <HeaderTitle title={visit.title} subtitle={visit.caption} />
