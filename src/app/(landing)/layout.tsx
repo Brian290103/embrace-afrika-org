26e4701a5d0d5c7742ac3d4250bb5f9bbcc6f6e0
Brian Saitabau <briansaitabau29@gmail.com>
2025-05-08 02:11:58 +0300

    added google analytics, testing out the tag

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -5,10 +5,24 @@ import Footer from "@/app/(landing)/components/footer";
 import MobileNav from "@/app/(landing)/components/mobile-nav";
 import { Toaster } from "sonner";
 import WhatsappButton from "@/app/(landing)/components/whatsapp-button";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <div className={"font-body"}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y8PMFBYHVQ"
+      ></Script>
+      <Script id={"google-analytics"}>
+        {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+    
+                gtag('config', 'G-Y8PMFBYHVQ'); 
+            `}
+      </Script>
       <Toaster richColors={true} position={"top-center"} />
       <TopNavbar />
       <Navbar />
